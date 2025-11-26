c5aaa720bba719407335f43585ef4f27ce1abec3
abdelrahmanELBORGY <abdelrahmanmoatazfouad@gmail.com>
2025-11-26 22:36:54 +0200

    Created another main file and renamed the previous main to be just for the prototype we implemented under the name "prototype_main".

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:intake/main.dart';
+import 'package:intake/prototype_main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
