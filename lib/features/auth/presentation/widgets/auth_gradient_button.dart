a1dc98318bbc3dfa1cdbf219843091d256f594d5
abdelrahmanELBORGY <abdelrahmanmoatazfouad@gmail.com>
2025-12-08 23:25:37 +0200

    Created SignUp Page with all its dependencies.

---
+++ lib/features/auth/presentation/widgets/auth_gradient_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/theme/appPalette.dart';
+
+class AuthGradientButton extends StatelessWidget {
+  const AuthGradientButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+  return Container(
+    decoration: BoxDecoration(
+      gradient: const LinearGradient(
+          colors: [
+            AppPallete.gradient1,
+            AppPallete.gradient2,
+          ],
+        begin: Alignment.topLeft,
+        end: Alignment.topRight
+      ),
+      borderRadius: BorderRadius.circular(7),
+    ),
+
+    child: ElevatedButton(
+        onPressed: (){},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppPallete.transparentColor,
+          shadowColor: AppPallete.transparentColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          fixedSize: const Size(395, 55),
+        ),
+        child: Text("Sign Up", style: TextStyle(
+          fontSize: 17,
+          fontWeight: FontWeight.w600,
+        ),),
+    ),
+  );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:intake/prototype_main.dart';
 
 void main() {
