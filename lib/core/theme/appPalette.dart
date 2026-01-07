910424a958735efd8668f3421ff96b01aa96ce75
abdelrahmanELBORGY <abdelrahmanmoatazfouad@gmail.com>
2026-01-07 22:30:07 +0200

    Modified Colors to follow the Application's Color Template.

--- lib/core/theme/appPalette.dart
+++ lib/core/theme/appPalette.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
 class AppPallete {
-  static const Color backgroundColor = Color(0xFF01252E);
+  static const Color backgroundColor = Color(0xFF232323);
   static const Color gradient1 = Color.fromRGBO(187, 63, 221, 1);
   static const Color gradient2 =  Color.fromRGBO(251, 109, 169, 1);
   static const Color gradient3 = Color(0xFF0097B2);
   static const Color borderColor = Color.fromRGBO(52, 51, 67, 1);
-  static const Color widgetColor = Color(0xFF0097B2);
+  static const Color widgetColor = Color(0xFF896CFE);
   static const Color textColor_dark = Colors.black;
-  static const Color textColor_light = Colors.white;
-  static const Color selectedColor = Color(0xFF0097B2);
+  static const Color textColor_light = Color(0xFF232323);
+  static const Color selectedColor = Color(0xFFE2F163);
   static const Color whiteColor = Colors.white;
   static const Color greyColor = Colors.grey;
   static const Color errorColor = Colors.redAccent;
